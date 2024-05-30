431f7fe463302a50e0ae463ee2bb708d3c8d3e05
William ☕️ <113069637+williamx0@users.noreply.github.com>
2024-05-30 15:03:13 +0100

    added new js classes

--- react-spring/src/App.jsx
+++ react-spring/src/App.jsx
@@ -1,7 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
+import logo from './logo.svg'
+
 
 const App = () => {
 
